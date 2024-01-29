ca6f6801a235a95bfde4b832361ac8d712f4b5e0
CosmoStarPL <cosmostarpl@gmail.com>
2024-01-29 17:36:19 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/index.css', './src/pages/Contacts/index.jsx', './src/pages/Contacts/EditModal.jsx', './src/root/index.jsx', './src/components/CreateContact.jsx'];
+export const theme = {
+  extend: {
+    height: {
+      "half": "74vh"
+    }
+  },
+};
+export const plugins = [];
+
